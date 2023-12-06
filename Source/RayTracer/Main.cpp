2a4eb793191cf0ba1808e2ed9c48ff756f233e4c
Kol4mo <47543776+kol4mo@users.noreply.github.com>
2023-12-06 08:51:57 -0700

    added dielectric and emissive materials

--- Source/RayTracer/Main.cpp
+++ Source/RayTracer/Main.cpp
@@ -8,6 +8,7 @@
 #include "Sphere.h"
 #include "Plane.h"
 #include <memory>
+#include <glm/gtx/color_space.hpp>
 int main(int, char**) {
 	std::cout << "Hello World!\n";
 
@@ -16,28 +17,42 @@ int main(int, char**) {
 	renderer.CreateWindow("hi", 400, 300);
 	Canvas canvas(400, 300, renderer);
 	float aspectRatio = canvas.GetSize().x / (float)canvas.GetSize().y;
-	std::shared_ptr<Camera> camera = std::make_shared<Camera>(glm::vec3{ 0, 0, 1 }, glm::vec3{ 0, 0, 0 }, glm::vec3{ 0, 1, 0 }, 70.0f, aspectRatio);
+	std::shared_ptr<Camera> camera = std::make_shared<Camera>(glm::vec3{ 0, 1, 10 }, glm::vec3{ 0, 0, 0 }, glm::vec3{ 0, 1, 0 }, 20.0f, aspectRatio);
 
-	Scene scene(20); // sky color could be set with the top and bottom color
+	Scene scene(5, glm::vec3{ 1.0f }, glm::vec3{ 0.5f, 0.7f, 1.0f });// sky color could be set with the top and bottom color
 	scene.SetCamera(camera);
 
 	// create material
-	auto lambertian = std::make_shared<Lambertian>(color3_t{ 0, 0, 1 });
-	auto metal = std::make_shared<Metal>(color3_t{ 1, 1, 1 }, 0.0f);
-	auto materialp = std::make_shared<Lambertian>(color3_t{ 1 , 0, 1 });
-	auto plane = std::make_unique<Plane>(glm::vec3{ 0, -3, 0 }, glm::vec3{ 0, 1, 0 }, materialp);
+	auto material = std::make_shared<Lambertian>(color3_t{ 0.2f });
+	auto plane = std::make_unique<Plane>(glm::vec3{ 0, 0, 0 }, glm::vec3{ 0, 1, 0 }, material);
 	scene.AddObject(std::move(plane));
 	// create objects -> add to scene
-	for (int i = 0; i < 10; i++)
+	for (int x = -10; x < 10; x++)
 	{
-		std::shared_ptr<Material> material = (rand() % 2 == 0) ? std::dynamic_pointer_cast<Material>(lambertian) : std::dynamic_pointer_cast<Material>(metal);
-		auto sphere = std::make_unique<Sphere>(glm::vec3{ random(-10, 10), random(-1,-2), random(-2, -20)}, 1, material);
-		scene.AddObject(std::move(sphere));
+		for (int z = -10; z < 10; z++)
+		{
+
+			std::shared_ptr<Material> material;
+
+			// create random material
+			float r = random01();
+			if (r < 0.3f)		material = std::make_shared<Lambertian>(glm::rgbColor(glm::vec3{ random(0, 360), 1.0f, 1.0f }));
+			else if (r < 0.6f)	material = std::make_shared<Metal>(color3_t{ random(0.5f, 1.0f) }, random(0, 0.5f));
+			else if (r < 0.9f)	material = std::make_shared<Dielectric>(color3_t{ 1.0f }, random(1.1f, 2));
+			else				material = std::make_shared<Emissive>(glm::rgbColor(glm::vec3{ random(0, 360), 1.0f, 1.0f }), 5.0f);
+
+			// set random sphere radius
+			float radius = random(0.2f, 0.3f);
+			// create sphere using random radius and material
+			auto sphere = std::make_unique<Sphere>(glm::vec3{ x + random(-0.5f, 0.5f), radius, z + random(-0.5f, 0.5f) }, radius, material);
+			// add sphere to the scene
+			scene.AddObject(std::move(sphere));
+		}
 	}
 
 	// render scene 
 	canvas.Clear({ 0, 0, 0, 1 });
-	scene.Render(canvas, 50);
+	scene.Render(canvas, 100);
 	canvas.Update();
 
 	bool quit = false;
--- Source/RayTracer/Scene.cpp
+++ Source/RayTracer/Scene.cpp
@@ -60,24 +60,24 @@ color3_t Scene::Trace(const ray_t& ray, float minDistance, float maxDistance, ra
 		}
 	}
 
-	// if ray hit object, scatter (bounce) ray and check for next hit
-	if (rayHit)
+// if ray hit object, scatter (bounce) ray and check for next hit
+if (rayHit)
+{
+	ray_t scattered;
+	color3_t color;
+ 
+	// check if maximum depth (number of bounces) is reached, get color from material and scattered ray
+	if (depth > 0 && raycastHit.material->Scatter(ray, raycastHit, color, scattered))
 	{
-		ray_t scattered;
-		color3_t color;
-
-		// check if maximum depth (number of bounces) is reached, get color from material and scattered ray
-		if (depth > 0 && raycastHit.material->Scatter(ray, raycastHit, color, scattered))
-		{
-			// recursive function, call self and modulate (multiply) colors of depth bounces
-			return color * Trace(scattered, minDistance, maxDistance, raycastHit, depth - 1);
-		}
-		else
-		{
-			// reached maximum depth of bounces (color is black)
-			return color3_t{ 0, 0, 0 };
-		}
+		// recursive function, call self and modulate colors of depth bounces
+		return color * Trace(scattered, minDistance, maxDistance, raycastHit, depth - 1);
 	}
+	else
+	{
+		// reached maximum depth of bounces (get emissive color, will be black except for Emissive materials)
+		return raycastHit.material->GetEmissive();
+	}
+}
 
 	// if ray not hit, return scene sky color
 	glm::vec3 direction = glm::normalize(ray.m_direction);
